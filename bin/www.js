746ba7948ce84180e5baedd80fec493a45b2b1b7
Saddam-tech <salokhiddinov0727@gmail.com>
2022-12-03 23:55:17 +0900

    latest changes

--- bin/www.js
+++ bin/www.js
@@ -35,7 +35,7 @@ function onError(error) {
 function onListening() {
   var addr = server.address();
   var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-  debug("Listening on " + bind);
+  console.log("Listening on " + bind);
 }
 
 function normalizePort(val) {
